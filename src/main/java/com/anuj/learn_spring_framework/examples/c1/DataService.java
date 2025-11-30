317af7556f0fd47c3318dcddbc6a84f1532d103a
Anuj Gupta <apgupta3091@gmail.com>
2025-11-30 12:03:41 -0500

    Created classes with bus logic

---
+++ src/main/java/com/anuj/learn_spring_framework/examples/c1/DataService.java
@@ -0,0 +1,5 @@
+package com.anuj.learn_spring_framework.examples.c1;
+
+public interface DataService {
+    int[] retrieveData();
+}
---
+++ src/main/java/com/anuj/learn_spring_framework/examples/c1/MySQLDataService.java
@@ -0,0 +1,13 @@
+package com.anuj.learn_spring_framework.examples.c1;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("MySQLDataServiceQualifier")
+public class MySQLDataService implements DataService {
+    @Override
+    public int[] retrieveData() {
+        return new int[] {1, 2, 3, 4, 5};
+    }
+}
