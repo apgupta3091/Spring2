b87a8889d86f41639dbffc5f5269ba4ecf2b4895
Anuj Gupta <apgupta3091@gmail.com>
2025-11-29 20:42:36 -0500

    spring

---
+++ src/main/java/com/anuj/learn_spring_framework/game/GamingConsole.java
@@ -0,0 +1,8 @@
+package com.anuj.learn_spring_framework.game;
+
+public interface GamingConsole {
+    void up();
+    void down();
+    void left();
+    void right();
+}
---
+++ src/main/java/com/anuj/learn_spring_framework/game/PacmanGame.java
@@ -0,0 +1,22 @@
+package com.anuj.learn_spring_framework.game;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacmanGame implements GamingConsole {
+    public void up(){
+        System.out.println("Up");
+    }
+
+    public void down(){
+        System.out.println("down");
+    }
+
+    public void left(){
+        System.out.println("left");
+    }
+
+    public void right(){
+        System.out.println("right");
+    }
+}
